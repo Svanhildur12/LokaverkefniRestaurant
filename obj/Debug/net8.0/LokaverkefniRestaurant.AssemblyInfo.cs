c1d3f5439fdfe1ea4bc8708bcab53ad42a2e3703
Svanhildur12 <svanhildur12@gmail.com>
2024-11-30 20:33:18 +0000

    commit

--- obj/Debug/net8.0/LokaverkefniRestaurant.AssemblyInfo.cs
+++ obj/Debug/net8.0/LokaverkefniRestaurant.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LokaverkefniRestaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd7339929751bc67643734879029c86d41cc63c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039fd1bf9c7d8835ae9332b5b88b129b2a4da36e")]
 [assembly: System.Reflection.AssemblyProductAttribute("LokaverkefniRestaurant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LokaverkefniRestaurant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
